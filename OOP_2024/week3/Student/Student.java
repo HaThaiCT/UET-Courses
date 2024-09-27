7f42207fbef4044727a92c5992627b39e721c60a
HaThaiCT <cthtzzz@gmail.com>
2024-09-28 01:54:34 +0700

    update week 3 oop

--- OOP_2024/week3/Student/Student.java
+++ OOP_2024/week3/Student/Student.java
@@ -1,47 +1,100 @@
-
-
 public class Student {
-    private String name, id, group, email;
-    
+
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
+    /**
+     * Gets the student's name.
+     * 
+     * @return the name of the student.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the student's name.
+     * 
+     * @param name the new name of the student.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the student's ID.
+     * 
+     * @return the ID of the student.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets the student's ID.
+     * 
+     * @param id the new ID of the student.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Gets the student's group.
+     * 
+     * @return the group of the student.
+     */
     public String getGroup() {
         return group;
     }
 
+    /**
+     * Sets the student's group.
+     * 
+     * @param group the new group of the student.
+     */
     public void setGroup(String group) {
         this.group = group;
     }
 
+    /**
+     * Gets the student's email.
+     * 
+     * @return the email of the student.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the student's email.
+     * 
+     * @param email the new email of the student.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Default constructor. Initializes a student with default values.
+     */
     public Student() {
-        name = "Student";
-        id = "000";
-        group = "K62CB";
-        email = "uet@vnu.edu.vn";
+        this.name = "Student";
+        this.id = "000";
+        this.group = "K62CB";
+        this.email = "uet@vnu.edu.vn";
     }
 
+    /**
+     * Constructs a student with the specified name, ID, and email.
+     * The group is set to the default value "K62CB".
+     * 
+     * @param name  the name of the student.
+     * @param id    the ID of the student.
+     * @param email the email of the student.
+     */
     public Student(String name, String id, String email) {
         this.name = name;
         this.id = id;
@@ -49,10 +102,24 @@ public class Student {
         this.email = email;
     }
 
+    /**
+     * Constructs a student by copying another student's information.
+     * 
+     * @param s the student to copy.
+     */
     public Student(Student s) {
         this.name = s.name;
         this.id = s.id;
         this.group = s.group;
         this.email = s.email;
+    }
+
+    /**
+     * Returns the student's information in a formatted string.
+     * 
+     * @return the student's details as a string.
+     */
+    public String getInfo() {
+        return this.name + " - " + this.id + " - " + this.group + " - " + this.email;
     }
 }
--- OOP_2024/week3/Student/StudentManagement.java
+++ OOP_2024/week3/Student/StudentManagement.java
@@ -1,9 +1,17 @@
-
 public class StudentManagement {
 
-    Student[] students = new Student[100];
-    int numberStudents = 0;
+    public static Student[] students = new Student[100];
+    public static int numberStudents = 0;
+    static String[] groups = new String[100];
+    static int numberGroups = 0;
 
+    /**
+     * Checks if two students are in the same group.
+     *
+     * @param s1 the first student
+     * @param s2 the second student
+     * @return true if both students are in the same group, false otherwise
+     */
     public static boolean sameGroup(Student s1, Student s2) {
         if (s1.getGroup().equals(s2.getGroup())) {
             return true;
@@ -11,20 +19,95 @@ public class StudentManagement {
         return false;
     }
 
-    public void addStudent(Student newStudent) {
-        students[numberStudents] = newStudent;
-        numberStudents++;
-    }
-
-    public void removeStudent(String id) {
-        for(int i = 0; i < numberStudents; i++) {
-            if(students[i].getId().equals(id)) {
-                
+    /**
+     * Checks if the student has already appeared in the system.
+     *
+     * @param s the student to check
+     * @return true if the student has appeared, false otherwise
+     */
+    public static boolean appeared(Student s) {
+        for (int i = 0; i < numberStudents; i++) {
+            if (s.getId().equals(students[i].getId())) {
+                return true;
             }
         }
-    } 
+        return false;
+    }
 
+    /**
+     * Adds a new student to the system if they haven't appeared yet.
+     *
+     * @param newStudent the student to add
+     */
+    public static void addStudent(Student newStudent) {
+        if (appeared(newStudent) == true) {
+            return;
+        }
+        students[numberStudents] = newStudent;
+        numberStudents++;
+        for (int i = 0; i < numberGroups; i++) {
+            if (newStudent.getGroup().equals(groups[i])) {
+                return;
+            }
+        }
+        groups[numberGroups] = newStudent.getGroup();
+        numberGroups++;
+    }
+
+    /**
+     * Removes a student from the system by their ID.
+     *
+     * @param id the ID of the student to remove
+     */
+    public static void removeStudent(String id) {
+        for (int i = 0; i < numberStudents; i++) {
+            if (students[i].getId().equals(id)) {
+                if (numberStudents - i - 1 >= 0) {
+                    System.arraycopy(students, i + 1, students, i, numberStudents - i - 1);
+                }
+                numberStudents--;
+            }
+        }
+    }
+
+    /**
+     * Returns a string of all students grouped by their group.
+     *
+     * @return a formatted string with students grouped by group
+     */
+    public static String studentsByGroup() {
+        String answer = "";
+        for (int i = 0; i < numberGroups; i++) {
+            answer += groups[i] + "\n";
+            for (int j = 0; j < numberStudents; j++) {
+                if (students[j].getGroup().equals(groups[i])) {
+                    answer += students[j].getInfo() + "\n";
+                }
+            }
+        }
+        return answer;
+    }
+
+    /**
+     * The main method to test the StudentManagement class.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
-    }    
+        Student s1 = new Student("Nguyen Van An", "17020001", "17020001@vnu.edu.vn");
+        s1.setGroup("K62CC");
+        Student s2 = new Student("Nguyen Van B", "17020002", "17020002@vnu.edu.vn");
+        s2.setGroup("K62CC");
+        Student s3 = new Student("Nguyen Van C", "17020003", "17020003@vnu.edu.vn");
+        s3.setGroup("K62CB");
+        Student s4 = new Student("Nguyen Van D", "17020004", "17020004@vnu.edu.vn");
+        s4.setGroup("K62CB");
+
+        addStudent(s1);
+        addStudent(s2);
+        addStudent(s3);
+        addStudent(s4);
+
+        System.out.println(studentsByGroup());
+    }
 }
